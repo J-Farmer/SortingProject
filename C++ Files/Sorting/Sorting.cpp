f2e5791e562c1cc69588082572d5788b789328c7
Jake Farmer <jfarmer@ad.artsk12.org>
2019-01-29 15:18:52 -0600

    C++ Files

--- C++ Files/Sorting/Sorting.cpp
+++ C++ Files/Sorting/Sorting.cpp
@@ -1,15 +1,13 @@
 #include "Sorting.h"
-#include <iostream>
-void Sorting::swap(int &a, int &b)
+void Sorting::swap(int *a, int *b)
 {
-	int temp = a;
-	a = b;
-	b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 void Sorting::SelectionSort(int arr[], int n)
 {
-	std::cout << "Size of Array: " << n << std::endl;
 	int min_idx = 0;
 
 	for(int i = 0; i < n-1; i++)
@@ -20,23 +18,97 @@ void Sorting::SelectionSort(int arr[], int n)
 			if(arr[j] < arr[min_idx])
 			{
 				min_idx = j;
-				swap(arr[min_idx], arr[j]);
 			}
+			swap(&arr[min_idx], &arr[j]);
 		}
 	}
 }
 
 void Sorting::BubbleSort(int arr[], int n)
 {
-	
+	bool swapped;
+	for(int i = 0; i < n - 1; i++
+	{
+		swapped = false;
+		for(int j = 0; j < n - i - 1; j++)
+		{
+			if(arr[j] > arr[j+1])
+			{
+				swap(&arr[j],&arr[j+1]);
+				swapped = true;
+			}
+		}
+		if(swapped == false)
+			break;
+	}
 }
 
 void Sorting::CocktailSort(int arr[], int n)
 {
-
+	bool swapped = true;
+	int start = 0;
+	int end = n-1;
+	
+	while(swapped)
+	{
+		swapped = false;
+		
+		for(int i=start; i<end; i++)
+		{
+			if(a[i] > a[i+1])
+			{
+				swap(&a[i],&a[i+1]);
+				swapped=true;
+			}
+		}
+		if(!swapped)
+			break;
+		
+		end--;
+		
+		for(int i = end-1; i >= start; --i)
+		{
+			if(arr[i] > arr[i+1])
+			{
+				swap(&a[i],&a[i+1]);
+				swapped=true;
+			}
+		}
+		
+		start++;
+	}		
 }
 
 void Sorting::OddEvenSort(int arr[], int n)
+{
+	bool isSorted = false;
+	
+	while(!isSorted)
+	{
+		isSorted = true;
+		
+		for (int i=1; i<=n-2; i=i+2) 
+        { 
+            if (arr[i] > arr[i+1]) 
+             { 
+                swap(arr[i], arr[i+1]); 
+                isSorted = false; 
+              } 
+        } 
+  
+        // Perform Bubble sort on even indexed element 
+        for (int i=0; i<=n-2; i=i+2) 
+        { 
+            if (arr[i] > arr[i+1]) 
+            { 
+                swap(arr[i], arr[i+1]); 
+                isSorted = false; 
+            } 
+        } 
+    }	
+}
+
+void Sorting::readFile(char* file)
 {
 
 }
--- C++ Files/Sorting/Sorting.h
+++ C++ Files/Sorting/Sorting.h
@@ -1,11 +1,14 @@
+#include<iostream>
+#include<fstream>
 class Sorting
 {
 public:
+	char* readFile(char* file);
 	void SelectionSort(int arr[], int n);
 	void BubbleSort(int arr[], int n);
 	void CocktailSort(int arr[], int n);
 	void OddEvenSort(int arr[], int n);
 
 private:
-	void swap(int &a, int &b);
+	void swap(int *a, int *b);
 };
