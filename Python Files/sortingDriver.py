49149231ca3986a5b063b3b20c0a05a37d517903
Jake Farmer <jfarmer@ad.artsk12.org>
2019-01-29 09:34:21 -0600

    Looked at Odd Even Sort

--- Python Files/sortingDriver.py
+++ Python Files/sortingDriver.py
@@ -1,6 +1,6 @@
 from timeit import default_timer as timer
 from sortingAlgo import bubbleSort, selectionSort, cocktailSort, oddEvenSort
-from random import randint
+
 
 def formatTime(algorithm, time):
 	if(time > 60):
@@ -10,45 +10,36 @@ def formatTime(algorithm, time):
 		print("%s took: %f seconds!" %(algorithm, time))
 
 
-#Write your file I/O Code here!
-arr = []
-#for i in range(0,10000):
-#    arr.append(randint(-10000,10000))
 
+arr = []
 file = open("../random_numbers.txt")
 for lines in file:
     arr.append(int(lines))
 file.close()
 
-print("Sorting!")
 start = timer()
-bubbleSort(arr)#put your function here!
+bubbleSort(arr)
 end = timer()
-
 time = round(end-start,3)
 formatTime("Bubble Sort", time)
 
-
-print("Sorting!(Selection Sort)")
 ss = arr
 start = timer()
-selectionSort(ss)#put your function here!
+selectionSort(ss)
 end = timer()
 time = round(end-start,3)
 formatTime("Selection Sort", time)
 
-print("Sorting!(Cocktail Sort)")
 cs = arr
 start = timer()
-cocktailSort(cs)#put your function here!
+cocktailSort(cs)
 end = timer()
 time = round(end-start,3)
 formatTime("Cocktail Sort", time)
 
-print("Sorting!(Odd Even Sort)")
 oes = arr
 start = timer()
-oddEvenSort(oes)#put your function here!
+oddEvenSort(oes)
 end = timer()
 time = round(end-start,3)
 formatTime("Odd Even Sort", time)
