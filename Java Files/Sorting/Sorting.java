d53149f88790cfb319fe3161c0335370a3bcb03c
Jake Farmer <jfarmer@ad.artsk12.org>
2019-02-05 08:03:21 -0600

    Stuff Signed-off-by: Jake Farmer <jfarmer@ad.artsk12.org>

--- Java Files/Sorting/Sorting.java
+++ Java Files/Sorting/Sorting.java
@@ -1,9 +1,7 @@
+import java.io.*;
+
 public class Sorting{
-	public void swap(int x, int y){
-		Integer temp = x;
-		x = y;
-		y = temp;
-	}
+
 	public int[] bubbleSort(int[] arr){
 		int n = arr.length;
 		for(int i = 0; i < n-1; i++)
@@ -15,26 +13,45 @@ public class Sorting{
 				}
 		return arr;
 	}
+	
 
 	public static void main(String args[]){
 		Sorting s = new Sorting();
-		int[] arr = new int[Integer.parseInt(args[0])];
-
-		for(int i = 0; i<arr.length;i++)
-			arr[i] = (int)(Math.random() * Integer.parseInt(args[1])) + 1;
-		if(args[2].equals("Print")){
-			for(int i = 0; i < arr.length;i++)
-				System.out.print(arr[i]+" ");
+		int[] arr = new int[8977];
+		
+		try
+		{
+			File f = new File("../../random_numbers.txt");
+			BufferedReader is = new BufferedReader(new FileReader(f));
+			String num;
+			int index = 0;
+			while((num = is.readLine()) != null)
+			{
+				//System.out.println(num);
+				arr[index] = Integer.parseInt(num);
+				index++;
+			}
+			is.close(); 
 		}
-		System.out.println();
-		System.out.println();
+		catch(IOException io)
+		{
+			System.out.print(io.getStackTrace());
+			return; 
+		}
+		
 		System.out.println("Sorting!");
+		
+		long startTime = System.nanoTime();
 		s.bubbleSort(arr);
+		long end = System.nanoTime() - startTime;
+		
 		System.out.println();
-		if(args[2].equals("Print")){
-			for(int i = 0; i < arr.length; i++)
-				System.out.print(arr[i]+" ");
-		}
+		
+		System.out.println((double)end/1000000000);
+
+		for(int i = 0; i < arr.length; i=i+10)
+			System.out.print(arr[i]+" ");
+		
 		System.out.println();
 	}
 }
